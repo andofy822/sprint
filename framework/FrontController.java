a3ca93e4f011e3bc998ed83f863e035586ca6af1
andofy822 <andofy822@gmail.com>
2024-05-28 14:12:28 +0200

    ...
    ...

--- framework/FrontController.java
+++ framework/FrontController.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import java.net.URL;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,7 +20,6 @@ public class FrontController extends HttpServlet {
     // Ho an'ny sprint 1,2
     ArrayList<String> listController;
     HashMap<String,Mapping> dicoMapping = new HashMap<String,Mapping>() ;//Clé ny URL ,d mapping ny value
-    String baseUrl;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         processRequest(req, resp);
@@ -28,28 +28,35 @@ public class FrontController extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         processRequest(req, resp);
     }
-    protected void processRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+    protected void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
         PrintWriter out= resp.getWriter();
         // out.println("Voici les listes des controller");
         // for (int i = 0; i < listController.size() ; i++) {
         //     out.println(listController.get(i));
         // }
         boolean existMapping = false;
-        String urlTaper = req.getRequestURL().toString().split(baseUrl)[1];
-        out.println("L'url taper "+urlTaper);
-        for (String key : dicoMapping.keySet()) {
-            if (key.compareTo(urlTaper)==0) {
-                existMapping = true;
-                out.println("Key "+ key);
-                break;
+        try{
+            String urlTaper = req.getRequestURI().replace(req.getContextPath(),"");
+            out.println("L'url taper est"+ urlTaper);
+            for (String key : dicoMapping.keySet()) {
+                if (key.compareTo(urlTaper)==0) {
+                    existMapping = true;
+
+                    break;
+                }
+            }
+            if (existMapping) {
+                out.println( "Methode correpondant : "+ this.dicoMapping.get(urlTaper).getMethodName());
+                out.println( "Dans la classe : "+ this.dicoMapping.get(urlTaper).getClassName());
+                out.println(execute(dicoMapping, urlTaper));
+            }
+            else{
+                out.println("URL introuvable");
             }
         }
-        if (existMapping) {
-            out.println( "Methode correpondant : "+ this.dicoMapping.get(urlTaper).getMethodName());
-            out.println( "Dans la classe : "+ this.dicoMapping.get(urlTaper).getClassName());
-        }
-        else{
-            out.println("URL introuvable");
+        catch(Exception e){
+            System.out.println(e);
+            throw new ServletException(e);
         }
     }
 
@@ -57,18 +64,15 @@ public class FrontController extends HttpServlet {
     public void init() throws ServletException {
         super.init();
         try {
-            String name_package = getServletConfig().getInitParameter("packageController");
-            this.baseUrl = getServletConfig().getInitParameter("baseUrl");
+            ServletContext sc = getServletContext();
+            String name_package = sc.getInitParameter("chemin");
             this.listController =  this.getCtrlInPackage(name_package);
             System.out.println("Le size du HashMap "+dicoMapping.size());
         } catch (Exception e) {
-            // TODO: handle exception
             System.out.println(e);
         }
         
     }
-    
-    /* sprint1  */
     public ArrayList<String> getCtrlInPackage( String name_package) throws Exception {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         String path = name_package.replace(".", "/");
@@ -79,7 +83,7 @@ public class FrontController extends HttpServlet {
             if (packageFile.exists() && packageFile.isDirectory()) {
                 File[] files = packageFile.listFiles();
                 for (int i = 0; i < files.length; i++) {
-                    if (files[i].getName().endsWith(".class")) {
+                    if (files[i].isFile() && files[i].getName().endsWith(".class")) {
                         String temp_name_classe = files[i].getName().substring(0, files[i].getName().lastIndexOf("."));
                         Class temp_class = Class.forName( name_package +"."+ temp_name_classe);
                         Annote annotation = (Annote) temp_class.getAnnotation(Annote.class);
@@ -89,6 +93,10 @@ public class FrontController extends HttpServlet {
                         }
                         
                     }
+                    else if(files[i].isDirectory()){
+                        String newPackage = path + "." + files[i].getName();
+                        getCtrlInPackage(newPackage);
+                    }
                 }
             }
             
@@ -97,14 +105,25 @@ public class FrontController extends HttpServlet {
         return liste; 
         
     }
-    /* sprint2 */
     public void setDicoMapping(Class c){
         Method[] methodes = c.getMethods();
         for (int j = 0; j < methodes.length; j++) {
             Get annotGet = methodes[j].getAnnotation(Get.class); 
             if ( annotGet !=null ) {
-                dicoMapping.put(annotGet.url(), new Mapping( c.getSimpleName() , methodes[j].getName()));
+                dicoMapping.put(annotGet.url(), new Mapping( c.getName() , methodes[j].getName()));
             }
+        }
+    }
+    public String execute(HashMap<String,Mapping> dicoMapping,String urlTaper)throws Exception{
+        try{
+        Class c = Class.forName(dicoMapping.get(urlTaper).getClassName());
+        String methode = dicoMapping.get(urlTaper).getMethodName();
+        Method m = c.getMethod(methode);
+        Object o = m.invoke(c.newInstance());
+        return (String)o;
+        }
+        catch(Exception e){
+            throw e;
         }
     }
     
