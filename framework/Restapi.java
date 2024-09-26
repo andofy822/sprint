9d3ea48deffd9517f99d9e9bd0b29520d321ec33
andofy822 <andofy822@gmail.com>
2024-09-26 10:50:54 +0200

    api
    api

---
+++ framework/Restapi.java
@@ -0,0 +1,12 @@
+package framework;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME) // Specifie la durée de vie de l'annotation
+@Target(ElementType.METHOD) // Specifie où l'annotation peut être utilisé (dans ce cas , sur les classes)
+public @interface Restapi {
+    public String url(); 
+}
--- script/script.bat
+++ script/script.bat
@@ -10,7 +10,7 @@ set "destination=D:\Webdynamique\framework\lib"
 :: Créer une liste de tous les fichiers .java dans le répertoire src et ses sous-répertoires
 dir /s /B "%src%\*.java" > sources.txt
 :: Exécuter la commande javac
-javac -d "%src%" -cp "C:\tomcat\lib\servlet-api.jar" @sources.txt
+javac -d "%src%" -cp "C:\Users\nec\Documents\GitHub\sprint\lib\*" @sources.txt
 :: Supprimer les fichiers sources.txt et libs.txt après la compilation
 del sources.txt
 cd "%src%"
